b00008808ec08f2c59e6c7ac511b6041ec2d2772
Kalif Purce <kalifpurce@gmail.com>
2022-04-20 19:21:46 -0700

    added files and basic

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of this project?',
+'What is the discription of this project?',
+'Would you like to include a license for this project?',
+'Would you like to include contributors for this project?',
+'What is your github profile name?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
