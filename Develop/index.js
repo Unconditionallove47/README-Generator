386437d412d1909bf92029fa0392b86efd25a8f1
Kalif Purce <kalifpurce@gmail.com>
2022-04-21 17:08:19 -0700

    Basics added

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,34 @@
-// TODO: Include packages needed for this application
 
+// TODO: Include packages needed for this application
+const fs = require('fs');
+//fs for filesave
+const inquirer = require('inquirer');
+//inquirer for node
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of this project?',
-'What is the discription of this project?',
-'Would you like to include a license for this project?',
-'Would you like to include contributors for this project?',
-'What is your github profile name?'];
+const questions = [''];inquirer.prompt([
+    { 
+        type:"input",
+        name:"projectName",
+        message:"what is your projects name?"
+     },{
+        type:"input",
+        name:"description",
+        message:"What is a good beginning discription for your project"
+
+     },{
+        type:"list",
+        name:"gitHubLicense",
+        choices:["Mit, ,NONE"],
+        message:"Would you like to include a license for your project?"
+     },{
+        type:"input",
+        name:"Contribution",
+        message:"Did you have any contributors on your project?"
+     }
+  ]). then(function(answers){
+console.log(answers)
+
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
